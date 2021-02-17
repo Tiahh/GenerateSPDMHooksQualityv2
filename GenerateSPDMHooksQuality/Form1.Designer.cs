0ab4df7c4228915d7a86070271d7f7789803fd1b
Tiahh <ciao.negro.tiahhemergency@gmail.com>
2021-02-17 18:08:21 +0100

    Initial commit

---
+++ GenerateSPDMHooksQuality/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿
+namespace GenerateSPDMHooksQuality
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(446, 337);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(556, 308);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(649, 278);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Hook";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.Black;
+            this.textBox3.ForeColor = System.Drawing.Color.Lime;
+            this.textBox3.Location = new System.Drawing.Point(396, 160);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox3.Size = new System.Drawing.Size(157, 73);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "Class name...";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.Black;
+            this.textBox4.ForeColor = System.Drawing.Color.Lime;
+            this.textBox4.Location = new System.Drawing.Point(816, 160);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox4.Size = new System.Drawing.Size(157, 44);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.Text = "Offset...";
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.Color.Black;
+            this.checkBox1.ForeColor = System.Drawing.Color.Red;
+            this.checkBox1.Location = new System.Drawing.Point(420, 278);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(76, 19);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "isMethod";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Black;
+            this.textBox2.ForeColor = System.Drawing.Color.Lime;
+            this.textBox2.Location = new System.Drawing.Point(187, 337);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox2.Size = new System.Drawing.Size(157, 73);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "code...";
+            this.textBox2.Visible = false;
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.Color.Black;
+            this.textBox5.ForeColor = System.Drawing.Color.Lime;
+            this.textBox5.Location = new System.Drawing.Point(87, 160);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox5.Size = new System.Drawing.Size(157, 73);
+            this.textBox5.TabIndex = 2;
+            this.textBox5.Text = "Type...";
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.Color.Black;
+            this.textBox6.ForeColor = System.Drawing.Color.Lime;
+            this.textBox6.Location = new System.Drawing.Point(1088, 160);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox6.Size = new System.Drawing.Size(157, 44);
+            this.textBox6.TabIndex = 6;
+            this.textBox6.Text = "Value...";
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.BackColor = System.Drawing.Color.Black;
+            this.checkBox2.ForeColor = System.Drawing.Color.Red;
+            this.checkBox2.Location = new System.Drawing.Point(9, 185);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(63, 19);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "isStatic";
+            this.checkBox2.UseVisualStyleBackColor = false;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // textBox7
+            // 
+            this.textBox7.BackColor = System.Drawing.Color.Black;
+            this.textBox7.ForeColor = System.Drawing.Color.Lime;
+            this.textBox7.Location = new System.Drawing.Point(610, 160);
+            this.textBox7.Multiline = true;
+            this.textBox7.Name = "textBox7";
+            this.textBox7.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox7.Size = new System.Drawing.Size(157, 44);
+            this.textBox7.TabIndex = 4;
+            this.textBox7.Text = "has value...";
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1469, 684);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "S.P.D.M. Hook generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.TextBox textBox7;
+    }
+}
+
---
+++ GenerateSPDMHooksQuality/Form1.cs
@@ -0,0 +1,364 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GenerateSPDMHooksQuality
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bool gay = false;
+            string[] gang = { "int", "float", "bool", "double", "long"};
+            foreach (string item in gang)
+            {
+                if (item == textBox5.Text)
+                {
+                    gay = true;
+                }
+            }
+            if (!changed && !textBox3.Text.Contains(" ") && textBox4.Text.StartsWith("0x") && textBox6.Text != "Value..." && gay)
+            {
+                textBox1.Text = "";
+                LoadDina(0);
+            }
+            else if (changed && !textBox3.Text.Contains(" ") && textBox4.Text.StartsWith("0x"))
+            {
+                textBox1.Text = "";
+                LoadDina(1);
+            }
+            else
+            {
+                MessageBox.Show("Gay failed something like always.");
+            }
+        }
+        public void LoadDina(int whatCase)
+        {
+            if (whatCase == 0)
+            {
+
+                string[] valuegayyouorwhat = new string[255];
+                string secondgayvalue = "";
+                string tempShit0 = "";
+                string[] gay = new string[255];
+                int counting = 0;
+                string isItRequired = "";
+                string gangsheet = "";
+                string allTogether = "";
+                if (!string.IsNullOrEmpty(textBox7.Text))
+                {
+                    secondgayvalue = ", ";
+                    string[] gang = { "int", "float", "bool", "double", "long" };
+                    string[] kidnoob = textBox7.Text.Split(',');
+                    valuegayyouorwhat = kidnoob;
+
+                }
+                
+                for (int i = 0; i < valuegayyouorwhat.Length; i++)
+                {
+                    
+                        isItRequired = ", ";
+                   
+                        if (!changed2)
+                        {
+                        if (counting == 0)
+                        {
+                            gay[i] += " oldGang" + counting++ + secondgayvalue;
+                            tempShit0 += isItRequired + valuegayyouorwhat[i].ToString() + gay[i];
+                            allTogether += gay[i];
+                        }
+                        else
+                        {
+                            if (i != valuegayyouorwhat.Length-1)
+                            {
+                                gay[i] += " oldGang" + counting++ + secondgayvalue;
+                                tempShit0 += valuegayyouorwhat[i].ToString() + gay[i];
+                                allTogether += gay[i];
+                            }
+                            else
+                            {
+                                gay[i] += " oldGang" + counting++;
+                                tempShit0 += valuegayyouorwhat[i].ToString() + gay[i];
+                                allTogether += gay[i];
+                            }
+                        }
+                        }
+                        else
+                        {
+                        if (i != valuegayyouorwhat.Length-1 && counting != 0)
+                        {
+                            gay[i] += " oldGang" + counting++ + secondgayvalue;
+                            tempShit0 += valuegayyouorwhat[i].ToString() + gay[i];
+                            allTogether += gay[i];
+                        }
+                        else if (counting == 0 && i != valuegayyouorwhat.Length-1)
+                        {
+                            gay[i] += "oldGang" + counting++ + secondgayvalue;
+                            tempShit0 += valuegayyouorwhat[i].ToString() + " " + gay[i];
+                            allTogether += gay[i];
+                        }
+                        else
+                        {
+                            gay[i] += " oldGang" + counting++;
+                            tempShit0 += valuegayyouorwhat[i].ToString() + gay[i];
+                            allTogether += gay[i];
+                        }
+                        }
+                        
+                        
+                }
+                if (!changed2)
+                {
+                    textBox1.AppendText("bool boolean = false;" + Environment.NewLine + Environment.NewLine);
+                    textBox1.AppendText(textBox5.Text + " (*old_" + textBox3.Text + ")(void* instance" + tempShit0 + ");" + Environment.NewLine
+                        + textBox5.Text + " " + textBox3.Text + "(void* instance" + tempShit0 + ")"
+                        + Environment.NewLine + "{" + Environment.NewLine +
+                        "    if (instance != NULL)" + Environment.NewLine +
+                        "    {" + Environment.NewLine +
+                        "       if (boolean)" + Environment.NewLine +
+                        "       {" + Environment.NewLine +
+                        "       return " + textBox6.Text + ";" + Environment.NewLine +
+                        "       }" + Environment.NewLine +
+                        "    }" + Environment.NewLine +
+                        "    return old_" + textBox3.Text + "(instance" + isItRequired + allTogether + ");" + Environment.NewLine +
+                        "}" + Environment.NewLine + Environment.NewLine + "SPDMHook(libName, Obfuscator2Tiahh(OBFUSCATE(\u0022" + textBox4.Text + "\u0022)), (void*)" + textBox3.Text + ", (void**)&old_" + textBox3.Text + ");");
+
+                }
+                else
+                {
+                    textBox1.AppendText("bool boolean = false;" + Environment.NewLine + Environment.NewLine);
+                    textBox1.AppendText(textBox5.Text + " (*old_" + textBox3.Text + ")(" + tempShit0.Replace(", " + valuegayyouorwhat[0].ToString(), "") + ");" + Environment.NewLine
+                         + textBox5.Text + " " + textBox3.Text + "(" + tempShit0.Replace(", " + valuegayyouorwhat[0].ToString(), "") + ")"
+                         + Environment.NewLine + "{" + Environment.NewLine +
+                         "    if (boolean)" + Environment.NewLine +
+                         "    {" + Environment.NewLine +
+                         "       return " + textBox6.Text + ";" + Environment.NewLine +
+                         "    }" + Environment.NewLine +
+                         "    else" + Environment.NewLine +
+                         "    {" + Environment.NewLine +
+                         "    return old_" + textBox3.Text + "(" + allTogether + ");" + Environment.NewLine +
+                         "    }" + Environment.NewLine +
+                         "}" + Environment.NewLine + Environment.NewLine + "SPDMHook(libName, Obfuscator2Tiahh(OBFUSCATE(\u0022" + textBox4.Text + "\u0022)), (void*)" + textBox3.Text + ", (void**)&old_" + textBox3.Text + ");");
+                }
+            }
+            else
+            {
+                string[] valuegayyouorwhat = new string[255];
+                string secondgayvalue = "";
+                string tempShit0 = "";
+                string[] gay = new string[255];
+                int counting = 0;
+                string isItRequired = "";
+                string gangsheet = "";
+                string allTogether = "";
+                if (!string.IsNullOrEmpty(textBox7.Text))
+                {
+                    secondgayvalue = ", ";
+                    string[] gang = { "int", "float", "bool", "double", "long" };
+                    foreach (string item in gang)
+                    {
+
+                        string[] kidnoob = textBox7.Text.Split(',');
+                        valuegayyouorwhat = kidnoob;
+                    }
+                }
+                for (int i = 0; i < valuegayyouorwhat.Length; i++)
+                {
+
+                    isItRequired = ", ";
+
+                    if (!changed2)
+                    {
+                        if (counting == 0)
+                        {
+                            gay[i] += " oldGang" + counting++ + secondgayvalue;
+                            tempShit0 += isItRequired + valuegayyouorwhat[i].ToString() + gay[i];
+                            allTogether += gay[i];
+                        }
+                        else
+                        {
+                            if (i != valuegayyouorwhat.Length - 1)
+                            {
+                                gay[i] += " oldGang" + counting++ + secondgayvalue;
+                                tempShit0 += valuegayyouorwhat[i].ToString() + gay[i];
+                                allTogether += gay[i];
+                            }
+                            else
+                            {
+                                gay[i] += " oldGang" + counting++;
+                                tempShit0 += valuegayyouorwhat[i].ToString() + gay[i];
+                                allTogether += gay[i];
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (i != valuegayyouorwhat.Length - 1 && counting != 0)
+                        {
+                            gay[i] += " oldGang" + counting++ + secondgayvalue;
+                            tempShit0 += valuegayyouorwhat[i].ToString() + gay[i];
+                            allTogether += gay[i];
+                        }
+                        else if (counting == 0 && i != valuegayyouorwhat.Length - 1)
+                        {
+                            gay[i] += "oldGang" + counting++ + secondgayvalue;
+                            tempShit0 += valuegayyouorwhat[i].ToString() + " " + gay[i];
+                            allTogether += gay[i];
+                        }
+                        else
+                        {
+                            gay[i] += " oldGang" + counting++;
+                            tempShit0 += valuegayyouorwhat[i].ToString() + gay[i];
+                            allTogether += gay[i];
+                        }
+                    }
+
+
+                }
+                if (!changed2)
+                {
+                    textBox1.AppendText("bool boolean = false;" + Environment.NewLine + Environment.NewLine);
+                    textBox1.AppendText("void " + " (*old_" + textBox3.Text + ")(void* instance" + tempShit0 + ");" + Environment.NewLine
+                        + "void" + " " + textBox3.Text + "(void* instance" + tempShit0 + ")"
+                        + Environment.NewLine + "{" + Environment.NewLine +
+                        "    if (instance != NULL)" + Environment.NewLine +
+                        "    {" + Environment.NewLine +
+                        "        if (boolean)" + Environment.NewLine +
+                        "        {" + Environment.NewLine +
+                        "       " + "//Value..." + Environment.NewLine +
+                        "        }" + Environment.NewLine +
+                        "    }" + Environment.NewLine +
+                        "    old_" + textBox3.Text + "(instance" + isItRequired + gay[0] + gay[1] + gay[2] + ");" + Environment.NewLine +
+                        "}" + Environment.NewLine + Environment.NewLine + "SPDMHook(libName, Obfuscator2Tiahh(OBFUSCATE(\u0022" + textBox4.Text + "\u0022)), (void*)" + textBox3.Text + ", (void**)&old_" + textBox3.Text + ");");
+                }
+                else
+                {
+                    textBox1.AppendText("bool boolean = false;" + Environment.NewLine + Environment.NewLine);
+                    textBox1.AppendText("void*" + " (*old_" + textBox3.Text + ")(" + valuegayyouorwhat[0].ToString() + tempShit0.Replace(", " + valuegayyouorwhat[0].ToString(), "") + ");" + Environment.NewLine
+                         + "void*" + " " + textBox3.Text + "(" + tempShit0 + ")"
+                         + Environment.NewLine + "{" + Environment.NewLine +
+                         "    if (boolean)" + Environment.NewLine +
+                         "    {" + Environment.NewLine +
+                         "       " + "//Value..." + Environment.NewLine +
+                         "    }" + Environment.NewLine +
+                         "    else" + Environment.NewLine +
+                         "    {" + Environment.NewLine +
+                         "    old_" + textBox3.Text + "(" + gay[0].Replace(" ", "") + gay[1] + gay[2] + ");" + Environment.NewLine +
+                         "    }" + Environment.NewLine +
+                         "}" + Environment.NewLine + Environment.NewLine + "SPDMHook(libName, Obfuscator2Tiahh(OBFUSCATE(\u0022" + textBox4.Text + "\u0022)), (void*)" + textBox3.Text + ", (void**)&old_" + textBox3.Text + ");");
+                }
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox3.Text == "Class name..")
+            {
+                textBox3.Text = "";
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox4.Text == "Offset..")
+            {
+                textBox4.Text = "";
+            }
+        }
+        public static bool changed = false;
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            changed = !changed;
+            if (changed)
+            {
+               // textBox2.Visible = true;
+                textBox5.Visible = false;
+                textBox6.Visible = false;
+            }
+            else
+            {
+                //textBox2.Visible = false;
+                textBox5.Visible = true;
+                textBox6.Visible = true;
+            }
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            bool gay = false;
+            string[] gang = { "int", "float", "bool", "double", "long" };
+            foreach (string item in gang)
+            {
+                if (item == textBox5.Text)
+                {
+                    gay = true;
+                }
+            }
+            if (gay)
+            {
+                textBox5.ForeColor = Color.Red;
+            }
+            else
+            {
+                textBox5.ForeColor = Color.White;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        static bool changed2 = false;
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            changed2 = !changed2;
+           
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox7.Text == "Has value..")
+            {
+                textBox7.Text = "";
+            }
+            bool gay = false;
+            string[] gang = { "int", "float", "bool", "double", "long" };
+            foreach (string item in gang)
+            {
+                if (item == textBox5.Text)
+                {
+                    gay = true;
+                }
+            }
+            if (gay)
+            {
+                textBox5.ForeColor = Color.Red;
+            }
+            else
+            {
+                textBox5.ForeColor = Color.White;
+            }
+        }
+    }
+}
